b3da55300d9b9094d3a482ec7903258a9f88f51c
Jackson Ming Hu <huming2207@gmail.com>
2020-06-14 15:18:39 +1000

    schema: request: refine user schema

--- src/schemas/requests/UserAuthSchema.ts
+++ src/schemas/requests/UserAuthSchema.ts
@@ -1,6 +1,9 @@
 import { JSONSchema7 } from 'json-schema';
+import { RouteSchema } from 'fastify';
+import { SuccessResponseSchema } from '../responses/SuccessResponseSchema';
+import { ErrorSchema } from '../responses/ErrorResponseSchema';
 
-export const LoginFormSchema: JSONSchema7 = {
+const LoginFormSchema: JSONSchema7 = {
     $id: '#loginForm',
     type: 'object',
     properties: {
@@ -10,7 +13,7 @@ export const LoginFormSchema: JSONSchema7 = {
     required: ['username', 'password'],
 };
 
-export const RegisterFormSchema: JSONSchema7 = {
+const RegisterFormSchema: JSONSchema7 = {
     $id: '#regForm',
     type: 'object',
     properties: {
@@ -19,4 +22,20 @@ export const RegisterFormSchema: JSONSchema7 = {
         email: { type: 'string', format: 'email' },
     },
     required: ['username', 'password', 'email'],
+};
+
+export const UserLoginSchema: RouteSchema = {
+    body: RegisterFormSchema,
+    consumes: ['application/x-www-form-urlencoded'],
+    produces: ['application/json'],
+    description: 'Register a new user',
+    response: { 200: SuccessResponseSchema, ...ErrorSchema },
+};
+
+export const UserRegisterSchema: RouteSchema = {
+    body: LoginFormSchema,
+    consumes: ['application/x-www-form-urlencoded'],
+    produces: ['application/json'],
+    description: 'User login, and get a new JWT token',
+    response: { 200: SuccessResponseSchema, ...ErrorSchema },
 };
