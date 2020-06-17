f70a7a98c77254cc795312a8e49d610ecf146c0b
Jackson Ming Hu <huming2207@gmail.com>
2020-06-17 22:01:50 +1000

    model: project: add kanban relationship

--- src/models/ProjectModel.ts
+++ src/models/ProjectModel.ts
@@ -1,11 +1,13 @@
 import { Document, Schema, model, Types } from 'mongoose';
 import { UserDoc } from './UserModel';
+import { KanbanDoc } from './KanbanModel';
 
 export interface ProjectDoc extends Document {
     name: string;
     description: string;
     owner: UserDoc;
     members: UserDoc[];
+    kanbans: KanbanDoc[];
 }
 
 export const ProjectSchema = new Schema({
@@ -13,6 +15,7 @@ export const ProjectSchema = new Schema({
     description: { type: String },
     owner: { type: Types.ObjectId, ref: 'User' },
     members: [{ type: Types.ObjectId, ref: 'User' }],
+    kanbans: [{ type: Types.ObjectId, ref: 'Kanban' }],
 });
 
 export default model<ProjectDoc>('Project', ProjectSchema);
