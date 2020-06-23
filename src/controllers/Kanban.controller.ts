b0373f61c8d602fb257d3bb97c915b1daf0fc140
Jackson Ming Hu <huming2207@gmail.com>
2020-06-23 23:00:54 +1000

    kanban: implement kanban getter APIs

--- src/controllers/Kanban.controller.ts
+++ src/controllers/Kanban.controller.ts
@@ -1,20 +1,53 @@
 import { Controller, GET, POST, PUT, DELETE } from "fastify-decorators";
 import { ServerRequest, ServerReply } from "fastify";
+import { getUserFromReq } from "../common/UserFetcher";
+import Kanban, { KanbanDoc } from "../models/KanbanModel";
 
 @Controller({ route: "/api/kanban" })
 export default class KanbanController {
     @GET({ url: "/" })
-    getAllKanbans = async (req: ServerRequest, reply: ServerReply): Promise<void> => {};
+    getAllKanbans = async (req: ServerRequest, reply: ServerReply): Promise<void> => {
+        const user = await getUserFromReq(req);
+        const kanbans = await Kanban.find({ owner: user }).select(
+            "_id title description owner project -__v -events", // Filter out events (or it will be a super long response...)
+        );
+
+        reply.code(200).send({
+            message: "OK",
+            data: {
+                kanbans,
+            },
+        });
+    };
 
     @GET({ url: "/:kanbanId" })
-    getOneKanban = async (req: ServerRequest, reply: ServerReply): Promise<void> => {};
+    getOneKanban = async (req: ServerRequest, reply: ServerReply): Promise<void> => {
+        const user = await getUserFromReq(req);
+        const id = req.params["kanbanId"] as string;
+        const kanban = await Kanban.findOne({ owner: user, _id: id }).select(
+            "_id title description owner project events -__v",
+        );
+
+        reply.code(200).send({
+            message: "OK",
+            data: {
+                kanban,
+            },
+        });
+    };
 
     @POST({ url: "/" })
-    createKanban = async (req: ServerRequest, reply: ServerReply): Promise<void> => {};
+    createKanban = async (req: ServerRequest, reply: ServerReply): Promise<void> => {
+        const user = await getUserFromReq(req);
+    };
 
     @PUT({ url: "/:kanbanId" })
-    modifyKanban = async (req: ServerRequest, reply: ServerReply): Promise<void> => {};
+    modifyKanban = async (req: ServerRequest, reply: ServerReply): Promise<void> => {
+        const user = await getUserFromReq(req);
+    };
 
     @DELETE({ url: "/:kanbanId" })
-    deleteKanban = async (req: ServerRequest, reply: ServerReply): Promise<void> => {};
+    deleteKanban = async (req: ServerRequest, reply: ServerReply): Promise<void> => {
+        const user = await getUserFromReq(req);
+    };
 }
