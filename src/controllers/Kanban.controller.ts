965d15bf06ce2c940d54fe7b4e0b4881c540cde5
Jackson Ming Hu <huming2207@gmail.com>
2020-06-30 22:12:57 +1000

    controller: make it to be protected

--- src/controllers/Kanban.controller.ts
+++ src/controllers/Kanban.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, GET, POST, PUT, DELETE } from "fastify-decorators";
 import { ServerRequest, ServerReply } from "fastify";
 import { getUserFromReq } from "../common/UserFetcher";
-import Kanban, { KanbanDoc } from "../models/KanbanModel";
+import Event from "../models/EventModel";
+import Kanban from "../models/KanbanModel";
+import { UserDoc } from "../models/UserModel";
+import { NotFoundError } from "../common/Errors";
+import BaseProtectedController from "./BaseProtected.controller";
 
 @Controller({ route: "/api/kanban" })
-export default class KanbanController {
+export default class KanbanController extends BaseProtectedController {
     @GET({ url: "/" })
     getAllKanbans = async (req: ServerRequest, reply: ServerReply): Promise<void> => {
         const user = await getUserFromReq(req);
@@ -20,10 +24,10 @@ export default class KanbanController {
         });
     };
 
-    @GET({ url: "/:kanbanId" })
+    @GET({ url: "/:id" })
     getOneKanban = async (req: ServerRequest, reply: ServerReply): Promise<void> => {
         const user = await getUserFromReq(req);
-        const id = req.params["kanbanId"] as string;
+        const id = req.params["id"] as string;
         const kanban = await Kanban.findOne({ owner: user, _id: id }).select(
             "_id title description owner project events -__v",
         );
@@ -39,15 +43,79 @@ export default class KanbanController {
     @POST({ url: "/" })
     createKanban = async (req: ServerRequest, reply: ServerReply): Promise<void> => {
         const user = await getUserFromReq(req);
+        const color = req.body["color"] ? parseInt(req.body["color"]) : 0;
+        const title = req.body["title"] as string;
+        const kanban = await Kanban.create<{ title: string; color: number; owner: UserDoc }>({
+            title,
+            color,
+            owner: user,
+        });
+
+        reply.code(200).send({
+            message: "OK",
+            data: {
+                kanban,
+            },
+        });
     };
 
-    @PUT({ url: "/:kanbanId" })
+    @POST({ url: "/:id/event" })
+    addEvent = async (req: ServerRequest, reply: ServerReply): Promise<void> => {
+        const user = await getUserFromReq(req);
+        const id = req.params["id"] as string;
+        const eventId = req.body["event"] as string;
+
+        // Check if this event exists...
+        const event = await Event.findOne({ id: eventId, owner: user });
+        if (!event) throw new NotFoundError("Event not found");
+
+        // If event is valid (exist), continue to add
+        const kanban = await Kanban.findOne({ id, owner: user });
+        if (!kanban) throw new NotFoundError("Kanban not found");
+
+        await Kanban.updateOne(kanban, { $push: { events: event } });
+        reply.code(200).send({
+            message: "Event added",
+            data: {
+                event,
+                kanban,
+            },
+        });
+    };
+
+    @PUT({ url: "/:id" })
     modifyKanban = async (req: ServerRequest, reply: ServerReply): Promise<void> => {
         const user = await getUserFromReq(req);
+        const id = req.params["id"] as string;
+
+        const kanban = await Kanban.findOne({ id, owner: user });
+        if (!kanban) throw new NotFoundError("Kanban not found");
+        await Kanban.updateOne(kanban, {
+            title: req.body["title"] || kanban.title,
+            description: req.body["description"] || kanban.description,
+            color: req.body["color"] || kanban.color,
+        });
+
+        reply.code(200).send({
+            message: "Kanban modified",
+            data: {
+                id: kanban.id,
+            },
+        });
     };
 
-    @DELETE({ url: "/:kanbanId" })
+    @DELETE({ url: "/:id" })
     deleteKanban = async (req: ServerRequest, reply: ServerReply): Promise<void> => {
         const user = await getUserFromReq(req);
+        const id = req.params["id"] as string;
+        const kanban = await Kanban.findOne({ id, owner: user });
+        if (!kanban) throw new NotFoundError("Kanban not found");
+        await Kanban.deleteOne({ id, owner: user });
+        reply.code(200).send({
+            message: "Kanban deleted",
+            data: {
+                id: kanban.id,
+            },
+        });
     };
 }
