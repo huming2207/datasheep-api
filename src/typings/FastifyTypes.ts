2455c6cb513906f95d09f6f2bf5cbc27edaccf36
Jackson Ming Hu <huming2207@gmail.com>
2020-07-12 08:05:23 +1000

    Remove unused types

--- src/typings/FastifyTypes.ts
+++ src/typings/FastifyTypes.ts
@@ -1,13 +1,4 @@
-import {
-    FastifySchema,
-    RawServerBase,
-    RawRequestDefaultExpression,
-    RawReplyDefaultExpression,
-    FastifyLoggerInstance,
-    RawServerDefault,
-} from "fastify";
-
-import http from "http";
+import { FastifySchema } from "fastify";
 
 declare module "fastify" {
     export interface FastifyOasSchema extends FastifySchema {
