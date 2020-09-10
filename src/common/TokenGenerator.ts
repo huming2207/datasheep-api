d968ca37077ab1c40dd07038b6ca2250826fd9fc
Jackson Ming Hu <huming2207@gmail.com>
2020-09-10 23:00:42 +1000

    Add specific UserJwtToken type

--- src/common/TokenGenerator.ts
+++ src/common/TokenGenerator.ts
@@ -3,6 +3,12 @@ import { customAlphabet } from "nanoid";
 const lut = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
 const lutSupp = "!@#$%^&*()_+-|\"':;<>,./?`~[]{}";
 
+export interface UserJwtToken {
+    id: string;
+    username: string;
+    email: string;
+}
+
 export const generateDeviceToken = (): string => {
     const generate = customAlphabet(lut, 20);
     return generate();
